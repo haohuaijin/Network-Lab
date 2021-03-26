7e120e150ed8cf4dee2448e8fb8a2f0da249bcd6
hhj <1291536750@qq.com>
2021-03-25 22:22:30 -0700

    lab2 wrap test

--- libsponge/wrapping_integers.cc
+++ libsponge/wrapping_integers.cc
@@ -14,8 +14,8 @@ using namespace std;
 //! \param n The input absolute 64-bit sequence number
 //! \param isn The initial sequence number
 WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
-    DUMMY_CODE(n, isn);
-    return WrappingInt32{0};
+    uint32_t t = (n + isn.raw_value()) % (1ul << 32);
+    return WrappingInt32{t};
 }
 
 //! Transform a WrappingInt32 into an "absolute" 64-bit sequence number (zero-indexed)
@@ -29,6 +29,16 @@ WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
 //! and the other stream runs from the remote TCPSender to the local TCPReceiver and
 //! has a different ISN.
 uint64_t unwrap(WrappingInt32 n, WrappingInt32 isn, uint64_t checkpoint) {
-    DUMMY_CODE(n, isn, checkpoint);
-    return {};
+    if(n == isn) return static_cast<uint64_t>(0);
+    uint64_t t = n.raw_value() > isn.raw_value() ? (n-isn) : (n.raw_value()+(1ul<<32)-isn.raw_value());
+    uint64_t bias = checkpoint / (1ul<<32);
+    uint64_t res = t + bias * (1ul << 32);
+    if(res > checkpoint && res > (1ul<<32)) {
+        if(checkpoint-res+(1ul<<32) < res - checkpoint)
+            res -= (1ul << 32);
+    } else if(res < checkpoint) {
+        if(res+(1ul<<32)-checkpoint < checkpoint-res)
+            res += (1ul << 32);
+    }
+    return res;
 }
