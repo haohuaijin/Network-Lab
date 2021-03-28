0dcf77c64c4a56cae852d09e1e66ed90fa274a5f
hhj <1291536750@qq.com>
2021-03-28 11:15:20 +0800

    delete some useless code

--- libsponge/tcp_receiver.cc
+++ libsponge/tcp_receiver.cc
@@ -22,19 +22,11 @@ void TCPReceiver::segment_received(const TCPSegment &seg) {
         FIN = true;
     uint64_t index = unwrap(seg.header().seqno, ISN, checkpoint);
     _reassembler.push_substring(seg.payload().copy(), (index > 0 ? index - 1 : 0), FIN);
-    std::cout << "data: " << seg.payload().copy();
-    std::cout << " index: " << (index > 0 ? index - 1 : 0) << std::endl;
     checkpoint = index;
     if (seg.header().seqno == ACKNO || seg.header().syn) {
         uint32_t SF = (_reassembler.stream_out().input_ended() ? 1 : 0);
         ACKNO = wrap(_reassembler.index() + 1 + SF, ISN);
     }
-    /*
-    if(seg.header().seqno == ACKNO || seg.header().syn) {
-        uint32_t SF = (seg.header().syn ? 1 : 0) + (seg.header().fin ? 1 : 0);
-        ACKNO = WrappingInt32(seg.payload().size() + seg.header().seqno.raw_value() + SF);
-    }
-    */
 }
 
 optional<WrappingInt32> TCPReceiver::ackno() const {
--- writeups/lab2.md
+++ writeups/lab2.md
@@ -5,18 +5,15 @@ My name: [your name here]
 
 My SUNet ID: [your sunetid here]
 
-This lab took me about [n] hours to do. I [did/did not] attend the lab session.
+This lab took me about [9] hours to do. I [did/did not] attend the lab session.
 
 I worked with or talked about this assignment with: [please list other sunetids]
 
-Program Structure and Design of the TCPReceiver and wrap/unwrap routines:
-[]
+##### Program Structure and Design of the TCPReceiver and wrap/unwrap routines:
 
-Implementation Challenges:
-[]
+##### Implementation Challenges:
 
-Remaining Bugs:
-[]
+##### Remaining Bugs:
 
 - Optional: I had unexpected difficulty with: [describe]
 
